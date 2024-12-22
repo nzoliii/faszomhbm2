2d5f7fbd7389552563c1f7919c244e8a0b497e46
nzoliii <nemethzoltanjozsef@gmail.com>
2024-12-22 16:18:17 +0100

    i changed my mind

--- src/main/java/com/hbm/lib/RefStrings.java
+++ src/main/java/com/hbm/lib/RefStrings.java
@@ -3,13 +3,16 @@ package com.hbm.lib;
 public class RefStrings {
 	public static final String MODID = "hbm";
 	public static final String NAME = "FaszomHBM 2";
-	public static final String VERSION = "RELEASE-1";
-	public static final String CHANGELOG = "Using NTM-Extended-1.12.2-2.0.2 as a base, everything that was FaszomHBM has been completely rewritten from scratch, and now its called FaszomHBM 2. $You can find the full FaszomHBM 2 Roadmap on GitHub: $https://github.com/users/nzoliii/projects/3/";
-	//HBM's Beta Naming Convention:
+	public static final String VERSION = "Son-of-a-Bitch";
+	public static final String CHANGELOG = "- Check GitHub for changelog. $- https://github.com/users/nzoliii/projects/3";	//HBM's Beta Naming Convention:
+
+	// The Chagelog when this will finally release on Curseforge: public static final String CHANGELOG = "Using NTM-Extended-1.12.2-2.0.2 as a base, everything that was FaszomHBM has been completely rewritten from scratch, and now its called FaszomHBM 2. $You can find the full FaszomHBM 2 Roadmap on GitHub: $https://github.com/users/nzoliii/projects/3/";
+
 	//V T (X)
 	//V -> next release version
 	//T -> build type
 	//X -> days since 10/10/10
+
 	//Drillgon200: I completely ignored this to make my own even worse naming system. Sigh.
 	public static final String CLIENTSIDE = "com.hbm.main.ClientProxy";
 	public static final String SERVERSIDE = "com.hbm.main.ServerProxy";
